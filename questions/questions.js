f80e3e9ad66464875850a8c3e8f55c563c05184e
Daniel Ramos <dramos@campus-024-053.ucdavis.edu>
2023-06-05 16:11:28 -0700

    kafbfsa

--- questions/questions.js
+++ questions/questions.js
@@ -4,6 +4,12 @@
     console.log("js running");
 
     const susan1 = document.getElementById("susan1");
+    const susan2 = document.getElementById("susan2");
+    const response2 = document.getElementById("response2");
+    const question3 = document.getElementById("q3");
+    const learnMore = document.getElementById("learnmore");
+    const nextButton = document.getElementById("nextButton");
+
 
     document.getElementById("closeOverlay1").addEventListener("click", function(){
 
@@ -11,5 +17,21 @@
 
 
     })
+
+    learnMore.addEventListener("click", function(){
+
+        response2.className="hide";
+        susan2.className= "show";
+
+    });
+
+    nextButton.addEventListener("click", function(){
+
+        response2.className="hide";
+        question3.className="show";
+
+
+    })
+    ;
 
 })();
